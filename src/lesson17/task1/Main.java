6a31c64638bb57b7f9d7347a1b2727ddf204abff
unknown <nvoloshchuk19@gmail.com>
2022-12-11 20:53:22 +0200

    leeson17

---
+++ src/lesson17/task1/Main.java
@@ -0,0 +1,18 @@
+package lesson17.task1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] num =  new Random().ints(5, -10,10).toArray();
+        System.out.println(Arrays.toString(num));
+        int sum = Arrays.stream(num).filter((number) -> number % 2 != 0).sum();
+        System.out.println(sum);
+        long count = Arrays.stream(num).filter((number) -> number % 2 == 0).count();
+        System.out.println(count);
+        int[] positive = Arrays.stream(num).filter((number)-> number>=0).toArray();
+        System.out.println(Arrays.toString(positive));
+    }
+}
---
+++ src/lesson17/task3/Main.java
@@ -0,0 +1,12 @@
+package lesson17.task3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> s = Arrays.asList("Highload", "High", "Load", "Highload");
+        long count = s.stream().filter(word -> word.toLowerCase().contains("high")).count();
+        System.out.println(count);
+    }
+}
